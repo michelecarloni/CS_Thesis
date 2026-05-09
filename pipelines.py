08ca07893c4b124641e573b6cc6d4cc5641492f8
michele <carlonimichele3008@gmail.com>
2026-05-09 11:01:15 +0200

    trying saga solver on logistic regression

--- pipelines.py
+++ pipelines.py
@@ -429,7 +429,8 @@ def pipeline_H2Crop_standard_ML_algo(save_results_dir, file_list, modality, load
     ml_models = {
         "decision_tree": DecisionTreeClassifier(random_state=42),
         "random_forest": RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1),
-        "logistic_regression": LogisticRegression(max_iter=2000, random_state=42),
+        # "logistic_regression": LogisticRegression(max_iter=2000, random_state=42),
+        "logistic_regression": LogisticRegression(max_iter=4000, solver='saga', C=0.1, random_state=42),
         "linear_svm": LinearSVC(max_iter=2000, dual=False, random_state=42)
     }
     
